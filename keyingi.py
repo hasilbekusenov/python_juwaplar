4df269011580e7a5dfff05eb6c153a2fc800fa33
Hasilbek Usenov <hasilbekreshala@gmail.com>
2024-12-09 21:54:04 +0500

    Update keyingi.py

--- keyingi.py
+++ keyingi.py
@@ -19,3 +19,18 @@ fruits[2] = "Banana"
 print(fruits)
 del fruits[2]
 print(fruits)
+
+"""
+next lesson
+"""
+
+info_people = {
+    "Joe" : "Iphone X",
+    "Xo" : "Redmi",
+    "Roy" : "Job"
+}
+print(info_people.items())
+for key, value in info_people.items():
+    print(f"Key is: {key}")
+    print(f"Value is: {value}\n")
+
